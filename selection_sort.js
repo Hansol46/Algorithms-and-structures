e5f766e84baf07392781961d3519c6715644d755
Yury Kiryaev <kiryaev.yury@yandex.ru>
2025-04-04 18:27:55 +0300

    Update selection_sort.js

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,17 @@
+const array = [-4, 1, 11, 7, 3, 9, -1, 0, 5, 13]
 
+function selectionSort(array){
+ for(let i = 0; i < array.length; i++) {
+   // Находим индекс минимального элемента в неотсортированной части
+   let minIndex = i; 
+   for(let j = i + 1; j < array.length; j++) { 
+     if(array[j] < array[minIndex]) {
+       minIndex = j
+     }
+   }
+   if(minIndex !== i) {
+     [array[i], array[minIndex]] = [array[minIndex], array[i]]
+   }
+ }
+ return array;
+}
