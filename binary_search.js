3357ea44d5b2465b3bbb47784835c9f91b4f53e9
Yury Kiryaev <kiryaev.yury@yandex.ru>
2025-04-04 17:59:40 +0300

    Update binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,17 +1,19 @@
 const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]; 
 
-function binarySearch(array, target){
- let left = 0;
- let right = array.length - 1;
+function binarySearch(array, target) {
+  let left = 0;
+  let right = array.length - 1;
 
- while(left <= right) {
-   let mid = Math.floor((left + right) / 2); 
-   if(array[mid] === target) return mid; 
-   if(array[mid] < target) { 
-     left = array[mid] + 1
-   } else {
-     right = array[mid] - 1
-   }
- }
- return -1
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (array[mid] === target) {
+      return array[mid];
+    }
+    if (array[mid] < target) {
+      left = mid + 1; 
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
 }
