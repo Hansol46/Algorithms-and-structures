4973ec487402c478da06d3f586ba35914c178d37
Yury Kiryaev <kiryaev.yury@yandex.ru>
2025-04-05 20:46:58 +0300

    Create tree(WIP).js

---
+++ tree(WIP).js
@@ -0,0 +1,51 @@
+const tree = [
+    {
+        v: 5,
+        c: [
+            {
+                v:10,
+                c: [
+                    {
+                        v:11,
+                    }
+                ]
+            },
+            {
+                v:7,
+                c: [
+                    {
+                        v:5,
+                        c: [
+                            {
+                                v:1
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        v: 5,
+        c: [
+            {
+                v:10
+            },
+            {
+                v:15
+            }
+        ]
+    }
+]
+
+function treeSumAllValues(tree) {
+  let sum = 0;
+  for(let child of tree){
+    sum += child.v;
+    if(!child.c) {
+      return child.v;
+    }
+    sum += treeSumAllValues(child.c)
+  }
+  return sum
+}
