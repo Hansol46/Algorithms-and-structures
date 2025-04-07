5596b7dca7da454a1d3065e388aea9d87e0c0159
Yury Kiryaev <kiryaev.yury@yandex.ru>
2025-04-07 20:10:48 +0300

    Update linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -1,7 +1,35 @@
 // Элемент связанного списка
 class Node {
-  constructor(value){
+  constructor(value) {
     this.value = value;
     this.next = null;
   }
-}
+};
+
+class LinkedList {
+  constuctor() {
+    this.size = 0;
+    this.root = null;
+  }
+
+  add(value) {
+    if(this.size === 0) {
+      this.root = new Node(value);
+      this.size += 1;
+      console.log('root', this.root);
+      return true;
+    }
+
+    // let node = this.root;
+    console.log('root', this.root);
+    
+    // let newNode = new Node(value);
+    // node.next = newNode;
+    // this.size += 1;
+  }
+
+  getSize() {
+    return this.size
+  }
+  
+};
