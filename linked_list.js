036e74b0e4798c97f1e1cb471a30974f883c8169
Yury Kiryaev <kiryaev.yury@yandex.ru>
2025-04-07 20:22:52 +0300

    Update linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -1,4 +1,3 @@
-// Элемент связанного списка
 class Node {
   constructor(value) {
     this.value = value;
@@ -7,7 +6,7 @@ class Node {
 };
 
 class LinkedList {
-  constuctor() {
+  constructor() {
     this.size = 0;
     this.root = null;
   }
@@ -16,16 +15,18 @@ class LinkedList {
     if(this.size === 0) {
       this.root = new Node(value);
       this.size += 1;
-      console.log('root', this.root);
       return true;
     }
 
-    // let node = this.root;
-    console.log('root', this.root);
-    
-    // let newNode = new Node(value);
-    // node.next = newNode;
-    // this.size += 1;
+    let node = this.root;
+
+    while(node.next) {
+      node = node.next();
+    }
+
+    let newNode = new Node(value);
+    node.next = newNode;
+    this.size += 1;
   }
 
   getSize() {
