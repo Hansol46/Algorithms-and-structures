873f3ea5d1d379f275b24b5109b3af2186ac06cb
Yury Kiryaev <kiryaev.yury@yandex.ru>
2025-04-08 17:02:47 +0300

    Update bubble_sort.js

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,5 +1,9 @@
 const array = [5,1,2,3,4,9,8,6,7] 
 
+/**
+ * Сложность алгоритма: O(n*n), 
+ * где n - количество элементов в массиве.
+ */
 function bubbleSort(array){
  for(let i = 0; i < array.length; i++) {
    for(let j = i + 1; j < array.length; j++){
